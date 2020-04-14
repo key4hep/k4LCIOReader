3e15951df9fb727b7fa3dff330d30a3941f2d716
zoujh <zoujh@ihep.ac.cn>
2020-04-14 14:21:13 +0800

    fix the dependencies for edm4hep

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 # get dependencies from cvmfs (available currently only for centos7)
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+
+export CMAKE_PREFIX_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/packageviews/edm4hep/latest/x86_64-centos7-gcc8-opt:$CMAKE_PREFIX_PATH
